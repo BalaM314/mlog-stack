b0af2bc40f5318a11e7cd5768b691d9712372b43
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2025-09-09 13:42:29 -0400

    allow reading files

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
-use itertools::Itertools;
+use std::{env::args, fs};
+
 
 mod lexer;
 mod parser;
 
 fn main() {
-	println!("Hello, world!");
+	if let Some(filename) = args().nth(1) {
+		match fs::read_to_string(filename) {
+			Ok(data) => {
+				let output = parser::parse(lexer::lexer(&data));
+				println!("{output}");
+			},
+			Err(err) => eprintln!("{err}"),
+		}
+	} else {
+		eprintln!("please specify a file to read");
+	}
 }
