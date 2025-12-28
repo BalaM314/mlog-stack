b6072d4182aa831b29e8f30dc93a87cec9ff3ad8
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2025-12-28 17:48:36 +0530

    improve demo main program

--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,11 @@ fn main() {
 fn run() -> Result<(), Box<dyn Error>> {
 	let filename = args().nth(1).ok_or("please specify a file to read")?;
 	let data = fs::read_to_string(filename)?;
+	println!("input:\n---\n{data}\n---\n");
 	let output = lexer::lexer(&data).and_then(parser::parse);
 	match output {
 		Ok(output) => {
+			println!("Compiling:\n---\n{output}\n---\n");
 			match &output {
 				parser::ASTBlock::Root { statements } => {
 					match statements.first() {
@@ -34,7 +36,6 @@ fn run() -> Result<(), Box<dyn Error>> {
 				},
 				_ => {}
 			}
-			println!("{output}")
 		},
 		Err(err) => eprintln!("{}", err.show(&data))
 	}
