98266906dd95baba44058a43dc273742c78d9994
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2025-12-28 23:45:54 +0530

    CLI

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,61 @@
-use std::{env::args, error::Error, fs};
+use std::{error::Error, fs, process::ExitCode};
+use clap::Parser;
+
+use crate::common::CError;
 
 mod lexer;
 mod parser;
 mod common;
 mod codegen;
 
-fn main() {
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+	#[arg()]
+	file_name: String,
+
+	#[arg(short, long, default_value = "false")]
+	verbose: bool,
+
+	#[arg(short, long, default_value = "-")]
+	output: String,
+}
+
+fn main() -> ExitCode {
 	match run() {
-		Err(err) => eprintln!("{err}"),
-		_ => {},
+		Err(err) => {
+			eprintln!("{err}");
+			ExitCode::FAILURE
+		},
+		_ => ExitCode::SUCCESS,
 	}
 }
 
 fn run() -> Result<(), Box<dyn Error>> {
-	let filename = args().nth(1).ok_or("please specify a file to read")?;
-	let data = fs::read_to_string(filename)?;
-	println!("input:\n---\n{data}\n---\n");
-	let output = lexer::lexer(&data).and_then(parser::parse);
-	match output {
+	let args = Args::parse();
+	let data = fs::read_to_string(args.file_name)?;
+	match compile(&data, args.verbose) {
 		Ok(output) => {
-			println!("Compiling:\n---\n{output}\n---\n");
-			let result = codegen::compile(output)?;
-			println!("{result}");
+			if args.output == "-" {
+				println!("{output}");
+			} else {
+				if args.verbose { eprintln!("{output}"); }
+				fs::write(args.output, output)?;
+			}
+			Ok(())
 		},
-		Err(err) => eprintln!("{}", err.show(&data))
+		Err(err) => Err(err.show(&data).into()),
 	}
-	Ok(())
-
 }
+
+fn compile(source:&str, verbose: bool) -> Result<String, CError> {
+	if verbose { eprintln!("input:\n---\n{source}\n---\n"); }
+	let tokens = lexer::lexer(source)?;
+	let ast = parser::parse(tokens)?;
+	if verbose { eprintln!("Compiling:\n---\n{ast}\n---\n"); }
+	let code = codegen::compile(ast)?;
+	Ok(code)
+}
+
 
